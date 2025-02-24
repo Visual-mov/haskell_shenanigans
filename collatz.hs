b32c125e5e945d7270703667bcb8bad023c2ca59
Visual-mov <ryandanver@gmail.com>
2025-02-24 15:36:00 -0800

    update

--- collatz.hs
+++ collatz.hs
@@ -1,13 +1,18 @@
 import System.IO
 import Data.List
 
-collatz :: Integer -> [String]
-collatz 1 = ["1"]
-collatz n = show n : collatz (if even n then n `div` 2 else 3*n+1)
+collatz :: Integer -> ([String], Int)
+collatz n = do
+    let list = collatzList n
+    (list, length list) where
+        collatzList 1 = ["1"]
+        collatzList n = show n : collatzList (if even n then n `div` 2 else 3*n+1)
 
 main :: IO ()
 main = do
     putStr "n: "
     hFlush stdout
     n <- getLine
-    putStrLn $ intercalate ", " (collatz (read n))
+    let (list, len) = collatz (read n)
+    putStrLn $ intercalate "," list
+    putStrLn $ "took " ++ show len ++ " steps"
--- rpn.hs
+++ rpn.hs
@@ -1,27 +1,43 @@
-type State = [Float]
+import System.IO
+import Data.Char
+
+-- reverse polish notation calculator
+
+type Stack = [Float]
 type BinOp = (Float -> Float -> Float) 
 
-doOp :: BinOp -> State -> State
+prompt :: String -> IO String
+prompt str = do
+    putStr str
+    hFlush stdout
+    getLine
+
+isFloat :: String -> Bool
+isFloat "" = False
+isFloat str = isDigit (head str)
+
+doOp :: BinOp -> Stack -> Stack
 doOp _ [] = []
 doOp _ [x] = error "operator requires two arguments"
 doOp f (x:y:tail) = y `f` x : tail
 
-rpn :: [String] -> State -> State
+rpn :: [String] -> Stack -> Stack
 rpn [] state = reverse state
 rpn (t:tail) state = rpn tail (case t of
         "+" -> doOp (+) state
         "-" -> doOp (-) state
         "*" -> doOp (*) state
         "/" -> doOp (/) state
-        _   -> read t : state
+        _   -> if isFloat t then read t : state
+               else error "unrecognized token"
     )
 
-main :: IO()
+main :: IO ()
 main = do
-    src <- getLine
-    if src == "q" then
+    line <- prompt ">> "
+    if line == "q" then
         return ()
     else do
-        let tokens = words src
+        let tokens = words line
         print $ rpn tokens []
         main
